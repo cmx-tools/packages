5c56368bb78b1130dfcd8545d481db2e9223de2b
Xiphe <github@xiphe.net>
2026-04-30 01:55:31 +0200

    docs: add full integraion example

---
+++ example/backend/api.tsx
@@ -0,0 +1,7 @@
+export type { Meta } from "@example/backend-contract";
+import type * as Contract from "@example/backend-contract";
+
+export const UserProfile: typeof Contract.UserProfile = async () => {
+  // fetch-user-data...
+  return <div>🧑‍🎤</div>;
+};
